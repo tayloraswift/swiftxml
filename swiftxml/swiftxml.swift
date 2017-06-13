7fe4878c1b3599da45f5022c03236ffa1b3ca93d
taylor swift <kelvinsthirteen@gmail.com>
2017-06-12 23:25:14 -0400

    refactor character classifiers into Unicode extension, improve behavior parsing comments, general code cleanup

--- swiftxml/swiftxml.swift
+++ swiftxml/swiftxml.swift
@@ -1,5 +1,5 @@
 /*
-    Copyright 2017, Kelvin Ma, kelvinsthirteen@gmail.com
+    Copyright 2017, Kelvin Ma (“taylorswift”), kelvin13ma@gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,115 +18,132 @@
 public
 protocol Parser
 {
-    func handle_data(data:[UnicodeScalar])
+    func handle_data(data:[Unicode.Scalar])
     func handle_starttag(name:String, attributes:[String: String])
     func handle_startendtag(name:String, attributes:[String: String])
     func handle_endtag(name:String)
     func error(_ message:String, line:Int, column:Int)
 }
 
-// NameStartChar  ::=   ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x2FF] | [#x370-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
-
-private
-func _is_NSC(_ us:UnicodeScalar) -> Bool
+extension Unicode.Scalar
 {
-    let u:UInt32 = us.value
-    return 0x61...0x7A ~= u || 0x41...0x5A ~= u // [a-z], [A-Z]
-    || u == 0x3A || u == 0x5F // ":", "_"
-    || 0xC0...0xD6 ~= u || 0xD8...0xF6 ~= u || 0xF8...0x2FF ~= u || 0x370...0x37D ~= u || 0x37F...0x1FFF ~= u || 0x200C...0x200D ~= u || 0x2070...0x218F ~= u || 0x2C00...0x2FEF ~= u || 0x3001...0xD7FF ~= u || 0xF900...0xFDCF ~= u || 0xFDF0...0xFFFD ~= u || 0x10000...0xEFFFF ~= u
-}
+    // NameStartChar  ::=   ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x2FF]
+    //                    | [#x370-#x37D]   | [#x37F-#x1FFF]  | [#x200C-#x200D] | [#x2070-#x218F]
+    //                    | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD]
+    //                    | [#x10000-#xEFFFF]
+    var is_xml_name_start:Bool
+    {
+        return "a" ... "z" ~= self || "A" ... "Z" ~= self // [a-z], [A-Z]
+        || self == ":" || self == "_" // ":", "_"
 
-// NameChar       ::=   NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
+        || "\u{C0}"   ... "\u{D6}"   ~= self || "\u{D8}"   ... "\u{F6}"   ~= self
+        || "\u{F8}"   ... "\u{2FF}"  ~= self || "\u{370}"  ... "\u{37D}"  ~= self
+        || "\u{37F}"  ... "\u{1FFF}" ~= self || "\u{200C}" ... "\u{200D}" ~= self
+        || "\u{2070}" ... "\u{218F}" ~= self || "\u{2C00}" ... "\u{2FEF}" ~= self
+        || "\u{3001}" ... "\u{D7FF}" ~= self || "\u{F900}" ... "\u{FDCF}" ~= self
+        || "\u{FDF0}" ... "\u{FFFD}" ~= self || "\u{10000}" ... "\u{EFFFF}" ~= self
+    }
 
-private
-func _is_NC(_ us:UnicodeScalar) -> Bool
-{
-    let u:UInt32 = us.value
-    return 0x61...0x7A ~= u || 0x41...0x5A ~= u // [a-z], [A-Z]
-    || u == 0x3A || u == 0x5F // ":", "_"
-    || 0x30...0x39 ~= u || u == 0x2D || u == 0x2E || u == 0xB7 // [0-9], "-", "."
-    || 0x0300...0x036F ~= u || 0x203F...0x2040 ~= u
-    || 0xC0...0xD6 ~= u || 0xD8...0xF6 ~= u || 0xF8...0x2FF ~= u || 0x370...0x37D ~= u || 0x37F...0x1FFF ~= u || 0x200C...0x200D ~= u || 0x2070...0x218F ~= u || 0x2C00...0x2FEF ~= u || 0x3001...0xD7FF ~= u || 0xF900...0xFDCF ~= u || 0xFDF0...0xFFFD ~= u || 0x10000...0xEFFFF ~= u
-}
 
-// S	   ::=   	(#x20 | #x9 | #xD | #xA)+
+    // NameChar       ::=   NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
+    var is_xml_name:Bool
+    {
+        return "a" ... "z" ~= self || "A" ... "Z" ~= self || "0" ... ":" ~= self
+        || self == "_" || self == "-" || self == "." || self == "\u{B7}"
 
-private
-func _is_whitespace(_ us:UnicodeScalar) -> Bool
-{
-    return us == "\u{20}" || us == "\u{9}" || us == "\u{D}" || us == "\u{A}"
-}
+        || "\u{0300}" ... "\u{036F}" ~= self || "\u{203F}" ... "\u{2040}" ~= self
 
-private
-enum _XStates
-{
-    case outer_save(UnicodeScalar), tag, e_tag, name_save(Character)
-    case c_exclam, c_hyphen1, comment, c_hyphen2, c_hyphen3
-    case ignore1, se_tag, attr_L(Character)
-    case ignore2, equals, ignore3, string, string_save(Character), store_attrib, ignore4
-    case invalid1(UnicodeScalar, Int, Int), invalid_etc
-    case emit(_Emission)
-}
-private
-enum _Emission
-{
-    case initial, start, start_end, end, empty(String, Int, Int), dropped(Int, Int), comment
+        || "\u{C0}"   ... "\u{D6}"   ~= self || "\u{D8}"   ... "\u{F6}"   ~= self
+        || "\u{F8}"   ... "\u{2FF}"  ~= self || "\u{370}"  ... "\u{37D}"  ~= self
+        || "\u{37F}"  ... "\u{1FFF}" ~= self || "\u{200C}" ... "\u{200D}" ~= self
+        || "\u{2070}" ... "\u{218F}" ~= self || "\u{2C00}" ... "\u{2FEF}" ~= self
+        || "\u{3001}" ... "\u{D7FF}" ~= self || "\u{F900}" ... "\u{FDCF}" ~= self
+        || "\u{FDF0}" ... "\u{FFFD}" ~= self || "\u{10000}" ... "\u{EFFFF}" ~= self
+    }
+
+    // S	   ::=   	(#x20 | #x9 | #xD | #xA)+
+    var is_xml_whitespace:Bool
+    {
+        return self == " " || self == "\u{9}" || self == "\u{D}" || self == "\u{A}"
+    }
 }
 
 public
 func parse(_ doc:String, parser:Parser)
 {
-    var data_buffer:[UnicodeScalar] = []
-    var str_buffer:String = ""
-    var label_buffer:String = ""
-    var name_buffer:String = ""
-    var attrib_buffer:[String: String] = [:]
+    enum XMLState
+    {
+        case outer_save(Unicode.Scalar), tag, e_tag, name_save(Unicode.Scalar)
+        case c_exclam, c_hyphen1, comment, c_hyphen2, c_hyphen3
+        case ignore1, se_tag, label(Unicode.Scalar)
+        case ignore2, equals, ignore3, string, string_save(Unicode.Scalar), store_attrib, ignore4
+        case invalid1(Unicode.Scalar, Int, Int), invalid_etc
+        case emit(AngleBracketGroup)
+    }
+
+    enum AngleBracketGroup
+    {
+        case initial,
+             start,
+             start_end,
+             end,
+             empty(String, Int, Int),
+             dropped(Int, Int),
+             comment
+    }
+
+    var data_buffer:[Unicode.Scalar] = [],
+        str_buffer:String            = "",
+        label_buffer:String          = "",
+        name_buffer:String           = "",
+        attributes:[String: String]  = [:]
 
     var context:String
     {
         return "attribute '\(label_buffer)' in tag '\(label_buffer)'"
     }
 
-    var l:Int = 0
-    var k:Int = 0
+    var l:Int = 0,
+        k:Int = 0
 
-    func emit_tag(_ emission:_Emission)
+    @inline(__always)
+    func _emit_tag(_ angle_bracket_group:AngleBracketGroup)
     {
-        switch emission
+        switch angle_bracket_group
         {
         case .initial:
             break
         case .start:
-            parser.handle_starttag(name: name_buffer, attributes: attrib_buffer)
+            parser.handle_starttag(name: name_buffer, attributes: attributes)
         case .start_end:
-            parser.handle_startendtag(name: name_buffer, attributes: attrib_buffer)
+            parser.handle_startendtag(name: name_buffer, attributes: attributes)
         case .end:
             parser.handle_endtag(name: name_buffer)
-        case let .empty(kind, l_1, k_1):
-            parser.error("empty tag '\(kind)' ignored", line: l_1, column: k_1)
-        case let .dropped(l_1, k_1):
-            parser.error("invalid tag '\(name_buffer)' dropped", line: l_1, column: k_1)
+        case let .empty(kind, l, k):
+            parser.error("empty tag '\(kind)' ignored", line: l, column: k)
+        case let .dropped(l, k):
+            parser.error("invalid tag '\(name_buffer)' dropped", line: l, column: k)
         case .comment:
             break
         }
     }
 
-    var state:_XStates = .emit(.initial)
-    var is_end_tag:Bool = false
-    var expected_str_delim:UnicodeScalar = "\""
-    var unexpected_str_delim:UnicodeScalar? = nil
+    var state:XMLState = .emit(.initial)
+    var inside_end_tag:Bool = false
+    var expected_str_delim:Unicode.Scalar = "\0"
+    var unexpected_str_delim:Unicode.Scalar? = nil
 
     for u in doc.unicodeScalars
     {
         switch state
         {
-        case .emit(let emission):
-            emit_tag(emission)
+        case .emit(let angle_bracket_group):
+            _emit_tag(angle_bracket_group)
 
             str_buffer = ""
             label_buffer = ""
             name_buffer = ""
-            attrib_buffer = [:]
+            attributes = [:]
 
             if u == "<"
             {
@@ -136,8 +153,8 @@ func parse(_ doc:String, parser:Parser)
             {
                 state = .outer_save(u)
             }
-        case .outer_save(let U):
-            data_buffer.append(U)
+        case .outer_save(let u_previous):
+            data_buffer.append(u_previous)
             if u == "<"
             {
                 state = .tag
@@ -149,11 +166,11 @@ func parse(_ doc:String, parser:Parser)
                 state = .outer_save(u)
             }
         case .tag:
-            is_end_tag = false
+            inside_end_tag = false
 
-            if _is_NSC(u)
+            if u.is_xml_name_start
             {
-                state = .name_save(Character(u))
+                state = .name_save(u)
             }
             else if u == "/"
             {
@@ -167,91 +184,91 @@ func parse(_ doc:String, parser:Parser)
             {
                 state = .emit(.empty("<>", l, k))
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
         case .e_tag:
-            is_end_tag = true
+            inside_end_tag = true
 
-            if _is_NSC(u)
+            if u.is_xml_name_start
             {
-                state = .name_save(Character(u))
+                state = .name_save(u)
             }
             else if u == ">"
             {
                 state = .emit(.empty("</>", l, k))
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
-        case .name_save(let C):
-            name_buffer.append(C)
-            if _is_NC(u)
+        case .name_save(let u_previous):
+            name_buffer.append(Character(u_previous))
+            if u.is_xml_name
             {
-                state = .name_save(Character(u))
+                state = .name_save(u)
             }
-            else if _is_whitespace(u)
+            else if u.is_xml_whitespace
             {
                 state = .ignore1
             }
-            else if !is_end_tag && u == "/"
+            else if !inside_end_tag && u == "/"
             {
-                state =  .se_tag
+                state = .se_tag
             }
             else if u == ">"
             {
-                state = .emit( is_end_tag ? .end : .start )
+                state = .emit(inside_end_tag ? .end : .start)
             }
             else
             {
                 state = .invalid1(u, l, k)
             }
         case .ignore1:
-            if _is_NSC(u)
+            if u.is_xml_name_start
             {
-                state = .attr_L(Character(u))
+                state = .label(u)
             }
-            else if !is_end_tag && u == "/"
+            else if !inside_end_tag && u == "/"
             {
-                state =  .se_tag
+                state = .se_tag
             }
             else if u == ">"
             {
-                state = .emit( is_end_tag ? .end : .start )
+                state = .emit(inside_end_tag ? .end : .start)
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
         case .se_tag:
             if u == ">"
             {
-                state = .emit( .start_end )
+                state = .emit(.start_end)
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
-        case .attr_L(let C):
-            label_buffer.append(C)
-            if _is_NC(u)
+        case .label(let u_previous):
+            label_buffer.append(Character(u_previous))
+            if u.is_xml_name
             {
-                state = .attr_L(Character(u))
+                state = .label(u)
             }
             else if u == "="
             {
                 state = .equals
             }
-            else if _is_whitespace(u)
+            else if u.is_xml_whitespace
             {
                 state = .ignore2
             }
             else if u == ">"
             {
                 parser.error("unexpected '>' after \(context)", line: l, column: k)
-                state = .emit( is_end_tag ? .end : .start )
+                state = .emit(inside_end_tag ? .end : .start)
             }
             else
             {
@@ -265,9 +282,9 @@ func parse(_ doc:String, parser:Parser)
             else if u == ">"
             {
                 parser.error("unexpected '>' after \(context)", line: l, column: k)
-                state = .emit( is_end_tag ? .end : .start )
+                state = .emit(inside_end_tag ? .end : .start)
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
@@ -277,14 +294,14 @@ func parse(_ doc:String, parser:Parser)
                 expected_str_delim = u
                 state = .string
             }
-            else if _is_whitespace(u)
+            else if u.is_xml_whitespace
             {
                 state = .ignore3
             }
             else if u == ">"
             {
                 parser.error("unexpected '>' after '=' on \(context)", line: l, column: k)
-                state = .emit( is_end_tag ? .end : .start )
+                state = .emit(inside_end_tag ? .end : .start)
             }
             else
             {
@@ -299,9 +316,9 @@ func parse(_ doc:String, parser:Parser)
             else if u == ">"
             {
                 parser.error("unexpected '>' after '=' on \(context)", line: l, column: k)
-                state = .emit( is_end_tag ? .end : .start )
+                state = .emit(inside_end_tag ? .end : .start)
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
@@ -313,81 +330,81 @@ func parse(_ doc:String, parser:Parser)
             }
             else
             {
-                state = .string_save(Character(u))
+                state = .string_save(u)
             }
-        case .string_save(let C):
-            str_buffer.append(C)
+        case .string_save(let u_previous):
+            str_buffer.append(Character(u_previous))
             if u == expected_str_delim
             {
                 state = .store_attrib
             }
             else
             {
-                state = .string_save(Character(u))
+                state = .string_save(u)
             }
         case .store_attrib:
-            attrib_buffer[label_buffer] = str_buffer
-            str_buffer = ""
+            attributes[label_buffer] = str_buffer
+            str_buffer   = ""
             label_buffer = ""
-            if _is_whitespace(u)
+            if u.is_xml_whitespace
             {
                 state = .ignore4
             }
-            else if !is_end_tag && u == "/"
+            else if !inside_end_tag && u == "/"
             {
                 state = .se_tag
             }
             else if u == ">"
             {
-                if is_end_tag
+                if inside_end_tag
                 {
                     parser.error("end tag '\(name_buffer)' cannot contain attributes", line: l, column: k)
-                    state = .emit( .end )
+                    state = .emit(.end)
                 }
                 else
                 {
-                    state = .emit( .start )
+                    state = .emit(.start)
                 }
             }
-            else if _is_NSC(u)
+            else if u.is_xml_name_start
             {
-                state = .attr_L(Character(u))
+                state = .label(u)
             }
             else
             {
                 state = .invalid1(u, l, k)
             }
         case .ignore4:
-            if _is_NSC(u)
+            if u.is_xml_name_start
             {
-                state = .attr_L(Character(u))
+                state = .label(u)
             }
-            else if !is_end_tag && u == "/"
+            else if !inside_end_tag && u == "/"
             {
                 state = .se_tag
             }
-            else if !_is_whitespace(u)
+            else if !u.is_xml_whitespace
             {
                 state = .invalid1(u, l, k)
             }
 
-        case let .invalid1(U, l_1, k_1):
+        case let .invalid1(u_previous, l_previous, k_previous):
             assert(unexpected_str_delim == nil)
-            if U == "\"" || U == "'"
+            if u_previous == "\"" || u_previous == "'"
             {
-                parser.error("unexpected string literal in tag '\(name_buffer)'", line: l_1, column: k_1)
-                if u != U
+                parser.error("unexpected string literal in tag '\(name_buffer)'", line: l_previous, column: k_previous)
+                if u != u_previous
                 {
-                 unexpected_str_delim = U
+                    unexpected_str_delim = u_previous
                 }
                 state = .invalid_etc
             }
             else
             {
-                parser.error("invalid character '\(U)' in tag '\(name_buffer)'", line: l_1, column: k_1)
+                parser.error("invalid character '\(u_previous)' in tag '\(name_buffer)'", line: l_previous, column: k_previous)
                 if u == ">"
                 {
-                    state = .emit( .dropped(l, k) )
+                    state = .emit(.dropped(l, k))
                 }
                 else
                 {
@@ -413,7 +430,7 @@ func parse(_ doc:String, parser:Parser)
             }
             else if u == ">"
             {
-                state = .emit( .dropped(l, k) )
+                state = .emit(.dropped(l, k))
             }
 
 
@@ -438,7 +455,7 @@ func parse(_ doc:String, parser:Parser)
             }
             else if u == ">"
             {
-                state = .emit(.empty("<!>", l, k))
+                state = .emit(.empty("<!->", l, k))
             }
             else
             {
@@ -461,14 +478,13 @@ func parse(_ doc:String, parser:Parser)
         case .c_hyphen3:
             if u == ">"
             {
-                state = .emit( .comment )
+                state = .emit(.comment)
             }
-            else
+            else if u != "-"
             {
                 state = .comment
             }
         }
-
 
         if u == "\n"
         {
@@ -483,10 +499,10 @@ func parse(_ doc:String, parser:Parser)
 
     switch state
     {
-    case.emit(let emission):
-        emit_tag(emission)
-    case .outer_save(let U):
-        data_buffer.append(U)
+    case .emit(let angle_bracket_group):
+        _emit_tag(angle_bracket_group)
+    case .outer_save(let u_last):
+        data_buffer.append(u_last)
         parser.handle_data(data: data_buffer)
     default:
         parser.error("unexpected EOF", line: l, column: k)
